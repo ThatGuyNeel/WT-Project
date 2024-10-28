2331d0edf69d00c5c5d9d625fdd7e122bb537a5c
ThatGuyNeel <143155611+ThatGuyNeel@users.noreply.github.com>
2024-10-28 13:12:23 +0530

    Update login.php

--- login.php
+++ login.php
@@ -18,11 +18,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     }
     
-    // Simple query without prepared statements
-    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$hashed_password'";
+    $user_table = "CREATE TABLE IF NOT EXISTS User (
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    )";
+    $r1=mysqli_query($conn,$user_table);
+	if($user_table)
+	{
+		echo "<br>'User' table created successfully";
+	}
+	else
+	{
+		echo "<br>Failed to create 'User' table";
+	}
+    // display
+    $sql = "SELECT * FROM User WHERE username = '$username' AND password = '$hashed_password'";
     $result = mysqli_query($conn, $sql);
     
-    // Check if login is successful
+   /* // Check if login is successful
     if (mysqli_num_rows($result) > 0) {
         echo "<script>
                 alert('Login successful! Redirecting to employee records.');
@@ -30,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
               </script>";
     } else {
         echo "<h2>Invalid username or password.</h2>";
-    }
+    } */
     
     mysqli_close($conn);
 }
